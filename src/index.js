a4b48aabb398936d69eda8e2969509254a8f4810
Lesedineo <133144071+Lesedineo@users.noreply.github.com>
2025-07-28 12:47:39 +0200

    Added API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+function searchCity(city) {
+  let apiKey = "db5o125ffe7254a099d635f35et51cb4";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault(); // Prevent the default form submission behavior
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value; // Set the city element's text to the input value
+  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 } // Log the input value to the console
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
